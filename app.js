edabac2e0f2c83cb68a38f15dc6ad6c7a408a218
Anton Papazov <antonp1p2@gmail.com>
2018-04-06 00:44:50 +0300

    latest

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 new Vue({
     el: "#exercise",
+
     data: {
-        name: "Anton",
-        age: "24",
-        imgLink: "http://sport360.com/wp-content/uploads/2018/01/1301liverpoolvmancity.jpg"
+        value: ""
     },
+
     methods:{
-        mutlipledAge: function(){
-            return this.age*3;
+        alertFunc: function() {
+            alert("someAlert");
         },
-        randomNum: function(){
-            var num = Math.floor((Math.random() * 100) + 1);
-            return num;
-        },
-        changeName: function(e){
-            this.name = e.target.value;
+        getValue: function(e) {
+            this.value = e.target.value;
         }
     }
 
